eca0ba749f41c39914e9050c83205deddb6e74e2
multiminparvo <15251670+multiminparvo@users.noreply.github.com>
2019-05-01 12:50:53 +0300

    Show data on HTML file
    We just make visible our rendered data by using HTML with @abdullahyldz . We need to edit for design and other functionality

--- practice-app/libs/create-exercise.js
+++ practice-app/libs/create-exercise.js
@@ -1,26 +1,50 @@
-const axios = require('axios');
-const querystring = require('querystring');
+var axios = require('axios');
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+var renderedArray = [];
 
-function getClassDataFromWikiData(classId, callback) {
-    const queryString = `SELECT ?item ?itemLabel ?pic WHERE { ?item wdt:P279 wd:${classId} . ?item wdt:P18 ?pic SERVICE wikibase:label { bd:serviceParam wikibase:language "en" } }`;
-
-    const url = 'https://query.wikidata.org/sparql';
+var getClassDataFromWikiData = ((classId, callback) => {
+    var queryString = `SELECT ?item ?itemLabel ?pic WHERE { ?item wdt:P279 wd:${classId} . ?item wdt:P18 ?pic SERVICE wikibase:label { bd:serviceParam wikibase:language "en" } }`;
+    var url = 'https://query.wikidata.org/sparql';
 
     axios.post(url,
-    querystring.stringify({
-        query: queryString,
-        format: "json"
-    }), {
-      headers: { 
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    }).then((res) => {
-        callback(res.data);
-    })
-    .catch((error) => {
-        console.error(error);
+        querystring.stringify({
+            query: queryString,
+            format: "json"
+        }), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then((res) => {
+            callback(res.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+});
+
+getClassDataFromWikiData('Q42889', (item) => {
+
+    var array = item.results.bindings;
+    array.forEach(element => {
+        var itemObject = {
+            name: element.itemLabel.value,
+            imgUrl: element.pic.value
+        }
+
+        renderedArray.push(itemObject);
     });
 
-}
+});
 
-module.exports = getClassDataFromWikiData;
+var server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(renderedArray));
+})
+
+setTimeout(() => {
+    server.listen(3000, '127.0.0.1');
+}, 3000);
+
+module.exports = getClassDataFromWikiData; 
