34d196bb25d6ed8419cb1dc50db47bf445bca5ff
multiminparvo <15251670+multiminparvo@users.noreply.github.com>
2019-05-05 00:49:43 +0300

    Definition is added again
    There might be better design for this code. Please dont hesitate to contribute it

--- practice-app/libs/create-exercise.js
+++ practice-app/libs/create-exercise.js
@@ -1,11 +1,42 @@
 var http = require("https");
 var wikiData = require('./getDataFromWikiData');
+var oxfordApi = require('./getDataFromOxfordApi');
 
 function createWordArray(callback) {
     var renderedArray = wikiData.generateRenderedArray();
+    var wordArray = [];
     setTimeout(() => {
-        callback(renderedArray);
+
+        parseEveryWordFromWikiData(renderedArray, (array) => {
+            wordArray = array;
+        })
+
+        setTimeout(() => {
+            console.log(wordArray)
+        }, 2000);
+
+        callback(wordArray);
     }, 2000);
+}
+
+function parseEveryWordFromWikiData(array, callback) {
+    wordArray = [];
+
+    array.forEach(element => {
+
+        setTimeout(() => {
+            oxfordApi.createRequestsForOxfordApi('en', element.name, element.imgUrl, (item) => {
+                oxfordApi.sendRequestForOxfordApi(item, function (wordObject) {
+                    wordArray.push(wordObject);
+
+                });
+            })
+
+        }, 200);
+    });
+
+    callback(wordArray);
+
 }
 
 function sendDataToLocalServer(callback) {
@@ -15,7 +46,6 @@ function sendDataToLocalServer(callback) {
 }
 
 sendDataToLocalServer((array) => {
-    console.log(array)
     var server = http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(array);
