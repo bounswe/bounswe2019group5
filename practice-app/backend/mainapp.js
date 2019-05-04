cf21f3f4fd5991820df26107621c6af07406449a
multiminparvo <15251670+multiminparvo@users.noreply.github.com>
2019-05-04 18:48:21 +0300

    All word objects unified in one array

--- practice-app/backend/mainapp.js
+++ practice-app/backend/mainapp.js
@@ -3,16 +3,37 @@ var wikiData = require('./getDataFromWikiData');
 
 //oxfordApi.createRequestsForOxfordApi('en','book',(item) => {oxfordApi.sendRequestForOxfordApi(item);});
 
-
 var renderedArray = wikiData.generateRenderedArray();
+var wordArray = [];
 setTimeout(() => {
 
     renderedArray.forEach(element => {
 
         setTimeout(() => {
-            oxfordApi.createRequestsForOxfordApi('en',element.name,element.imgUrl,(item) => {oxfordApi.sendRequestForOxfordApi(item);}) 
+            oxfordApi.createRequestsForOxfordApi('en',element.name,element.imgUrl,(item) => {oxfordApi.sendRequestForOxfordApi(item,function(wordObject){
+                wordArray.push(wordObject);
+
+            } );}) 
 
         }, 200);
-    });
-
+    });    
 }, 2000);
+
+
+setTimeout(() => {
+
+console.log(wordArray);
+
+}, 7000);
+
+function sendDataToLocalServer(callback) {
+    var server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(renderedArray));
+    })
+
+    setTimeout(() => {
+        server.listen(3000, '127.0.0.1');
+    }, 3000);
+}
+
