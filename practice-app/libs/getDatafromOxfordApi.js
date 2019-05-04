1b02c13901918248d38f0a36659e42e70c3b0ddf
multiminparvo <15251670+multiminparvo@users.noreply.github.com>
2019-05-04 23:28:55 +0300

    Update getDatafromOxfordApi.js
    The code is written according to import export conditions

--- practice-app/libs/getDatafromOxfordApi.js
+++ practice-app/libs/getDatafromOxfordApi.js
@@ -1,30 +1,51 @@
 var http = require("https");
-var app_id = "Will be taken from Database";
-var app_key = "Will be taken from Database";
 
-var wordId = "mosquito";
-var fields = "pronunciations";
-var strictMatch = "false";
+function createRequestsForOxfordApi(language, word, image, callback) {
+    var app_id = "d4ecea1c";
+    var app_key = "e18d3e367a757392aa6d916f78c655f1";
 
-var options = {
-    host: 'od-api.oxforddictionaries.com',
-    port: '443',
-    path: '/api/v2/entries/en-gb/' + wordId + '?fields=' + fields + '&strictMatch=' + strictMatch,
-    method: "GET",
-    headers: {
-        'app_id': app_id,
-        'app_key': app_key
-    }
+    var options = {
+        word: word,
+        image: image,
+        parameters: {
+            host: 'od-api.oxforddictionaries.com',
+            port: '443',
+            path: '/api/v1/entries/' + language + '/' + encodeURI(word) + '/definitions',
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'app_id': app_id,
+                'app_key': app_key
+            }
+        }
+    };
+    callback(options);
 };
 
-http.get(options, (resp) => {
-    let body = '';
-    resp.on('data', (data) => {
-        body += data;
-    });
-    resp.on('end', () => {
-        let parsed = JSON.parse(body);
+function sendRequestForOxfordApi(optionsParameters, callback) {
+    var parameters = optionsParameters.parameters || '';
+    var word = optionsParameters.word || '';
+    http.get(parameters, (resp) => {
+        var body = '';
+        resp.on('data', (data) => {
+            body += data;
+        });
+        resp.on('end', () => {
+            if (body.indexOf('<') !== 0) {
+                var parsed = JSON.parse(body || '{}');
 
-        console.log(parsed.results[0].lexicalEntries[0].pronunciations[0].audioFile);
+                var definition = ((((((((((parsed || {}).results || [])[0] || {}).lexicalEntries || [])[0] || {}).entries || [])[0] || {}).senses || [])[0] || {}).definitions || [])[0] || '';
+
+                var wordImage = optionsParameters.image;
+                var wordObject = {
+                    'word': word,
+                    'definition': definition,
+                    'image': wordImage
+                };
+                callback(wordObject);
+            }
+        });
     });
-});
+}
+module.exports.createRequestsForOxfordApi = createRequestsForOxfordApi;
+module.exports.sendRequestForOxfordApi = sendRequestForOxfordApi;
