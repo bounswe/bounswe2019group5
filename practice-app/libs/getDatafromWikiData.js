3b966d59dfa18950123dc8015459592eecd53e44
multiminparvo <15251670+multiminparvo@users.noreply.github.com>
2019-05-04 23:00:16 +0300

    Create getDatafromWikiData.js
    Move all codes to new branch

---
+++ practice-app/libs/getDatafromWikiData.js
@@ -0,0 +1,53 @@
+var axios = require('axios');
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+
+function getClassDataFromWikiData(classId, callback) {
+    var queryString = `SELECT ?itemLabel (SAMPLE(?item) AS ?item) (SAMPLE(?pic) AS ?pic) WHERE {  ?item wdt:P279 wd:${classId};    wdt:P18 ?pic.  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }}GROUP BY ?itemLabel LIMIT 10`;
+    var url = 'https://query.wikidata.org/sparql';
+
+    axios.post(url,
+        querystring.stringify({
+            query: queryString,
+            format: "json"
+        }), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then((res) => {
+            callback(res.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+
+function generateRenderedArray() {
+    var renderedArray = [];
+
+    getClassDataFromWikiData('Q42889', (item) => {
+
+
+        var array = item.results.bindings;
+
+        console.log('GENERATE')
+        array.forEach(element => {
+            var itemObject = {
+                name: element.itemLabel.value,
+                imgUrl: element.pic.value
+            }
+
+            renderedArray.push(itemObject);
+        });
+
+    });
+
+    return(renderedArray);
+}
+
+module.exports.getClassDataFromWikiData = getClassDataFromWikiData;  
+module.exports.generateRenderedArray = generateRenderedArray;  
+
+
+//module.exports.sendDataToLocalServer = sendDataToLocalServer; 
