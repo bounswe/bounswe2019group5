cd0d1006e6a1a7fbf199391296a35f11ec972943
Hasan Öztürk <32415369+hasan-ozturk-2017400258@users.noreply.github.com>
2019-05-09 20:23:07 +0300

    Home page gets general classes from wikidata

--- practice-app/app.js
+++ practice-app/app.js
@@ -6,6 +6,10 @@ var bodyParser = require('body-parser');
 var checkExercise = require('./libs/checkExercise');
 var cors = require('cors');
 
+var mysql      = require('mysql');
+var key = require('./credentials/key.json');
+
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -26,13 +30,56 @@ app.post('/deneme', function(req, res) {
     res.send({ message: 'req.body' });   
 });
 
-app.get('/home', function(req, res) {
+app.get('/home', async function(req, res) {
+
+    var promise = getLabelsFromDatabase();
+    answers = {};
+    await Promise.resolve(promise)
+    .then(res => answers = res)
+    .catch(error => console.log(error));
+    
+    console.log(JSON.parse(JSON.stringify(answers)))
+
+
+    //Change the key names in aligned with frontend
+    var myList = [];
+    for(i = 0;i< answers.length;i++){
+        obj = {};
+        obj['name'] = answers[i]['class_label'];
+        obj['id'] = answers[i]['class_id'];
+        myList.push(obj);
+    }
+    var pair_example = myList;
+    res.send(pair_example);  
+    /* This is format which is to be sent to frontend
     var pair_example = [
         { name: 'vehicle', id: 'Q42889' },
         { name: 'animal', id: 'Q729' },
-        { name: 'plant', id: 'Q756' }];
-    res.send(pair_example);   
+        { name: 'plant', id: 'Q756' }];*/ 
 });
+
+//gets all the class_ids and class_labels from the database
+async function getLabelsFromDatabase(){
+
+    var connection = mysql.createConnection(key);
+
+    var db = connection.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... nn");    
+    } else {
+        console.log(err);    
+    }
+    });
+    var sql = 'SELECT class_label, class_id FROM Class_Table';
+    return new Promise((resolve, reject) => {
+        connection.query(sql, function (err, result) {
+            if (err) throw err;
+            resolve(result);
+        }).on('error', function(e) {
+            reject(e.message);
+        });
+    }); 
+}
 
 app.post('/exercise',function(req,res){
     var exercise_example = getWordsOfAClass(req.body.id).then(result => res.send(result));
