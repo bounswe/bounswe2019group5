dd9e82a5d32ee24e004543f9dcd82f78a4697958
Hasan Öztürk <32415369+hasan-ozturk-2017400258@users.noreply.github.com>
2019-05-09 21:28:07 +0300

    deleted redundant code
    Apart from deleting the redundant code, I have exported the functions in case it is called outside.

--- practice-app/libs/getGeneralClasses.js
+++ practice-app/libs/getGeneralClasses.js
@@ -1,49 +1,8 @@
 var axios = require('axios');
 var querystring = require('querystring');
-//var http = require('http');
-//var fs = require('fs');
-//const wdk = require('wikidata-sdk')
-//var https = require('https')
-//var getJSON = require('get-json')
 var key = require('../credentials/key.json');
 var mysql      = require('mysql');
-
-function getClassDataFromWikiData(classId) {
-
-
-    var queryString = `SELECT ?itemLabel (SAMPLE(?item) AS ?item) (SAMPLE(?pic) AS ?pic) WHERE {  ?item wdt:P279 wd:${classId};    wdt:P18 ?pic.  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }}GROUP BY ?itemLabel LIMIT 30`;
-    var url = 'https://query.wikidata.org/sparql';
-
-    return axios.post(url,
-        querystring.stringify({
-            query: queryString,
-            format: "json"
-        }), {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-     });
-}
-
-async function generateRenderedArray(classId) {
-    var renderedArray = [];
-    //'Q42889'
-    try {
-        var res = await getClassDataFromWikiData(classId);
-        
-        var array = res.data.results.bindings;
-        array.forEach(element => {
-            var itemObject = {
-                name: element.itemLabel.value,
-                imgUrl: element.pic.value
-            }
-            renderedArray.push(itemObject);
-        });
-    } catch(error) {
-        console.log(error);
-    }
-    return renderedArray;
-}
+var wikiData = require('./getDatafromWikiData');
 
 //gets label given a class_id
 function wiker(class_id){
@@ -82,7 +41,6 @@ async function insertClass(class_id,class_label) {
     return new Promise((resolve, reject) => {
         connection.query(sql, values, function (err, result) {
             if (err) throw err;
-            //console.log("row inserted to table");
             resolve('item inserted');
         }).on('error', function(e) {
             reject(e.message);
@@ -91,13 +49,16 @@ async function insertClass(class_id,class_label) {
 }
 
 //This function returns items from wikidata which has 16 or more subclasses
-async function getter(){
+async function getter(item_num){
     var t=1;
-    for(i = 0;i < 10;i++){
+    for(i = 0;i < item_num;i++){
 
         var custom_class_id = 'Q' + t.toString();
 
-        var array = await generateRenderedArray(custom_class_id);
+        //var array = await generateRenderedArray(custom_class_id);
+        var array = await wikiData.generateRenderedArray(custom_class_id);
+
+
         console.log(custom_class_id + ": " + array.length);
         if(array.length > 16){
             var res = await wiker(custom_class_id);
@@ -111,9 +72,10 @@ async function getter(){
 }
 
 
-getter();
+getter(10);
 
-module.exports.getClassDataFromWikiData = getClassDataFromWikiData;  
-module.exports.generateRenderedArray = generateRenderedArray;  
+module.exports.getter = getter;
+module.exports.wiker = wiker;
+module.exports = insertClass = insertClass;
 
 
