8dcc15f240f5cefd8aaf71fadc531bcbb95496f8
Selamettin Dirik <selamettinn.95@gmail.com>
2019-05-12 05:37:14 +0300

    added informative feedback section

--- practice-app/libs/checkExercise.js
+++ practice-app/libs/checkExercise.js
@@ -4,34 +4,57 @@ var key = require('../credentials/key.json');
 
 
 async function checkExercise(exerciseId, givenAnswers, callback) {
+
     var promise = getExerciseAnswers(exerciseId);
     answers = {};
     await Promise.resolve(promise)
     .then(res => answers = res)
     .catch(error => console.log(error));
+
     console.log(answers);
     trueAnswers = 0;
 
+    let ret = giveFeedback(answers);
+
     if(answers.answer1 === givenAnswers[0]) {
         trueAnswers = trueAnswers+1;
+        ret[0]="correct";
     }
     if(answers.answer2 === givenAnswers[1]) {
         trueAnswers = trueAnswers+1;
+        ret[1]="correct";
     }
     if(answers.answer3 === givenAnswers[2]) {
         trueAnswers = trueAnswers+1;
+        ret[2]="correct";
     }
     if(answers.answer4 === givenAnswers[3]) {
         trueAnswers = trueAnswers+1;
+        ret[3]="correct";
     }
 
     falseAnswers = 4 - trueAnswers;
+
     response = {
         true: trueAnswers,
-        false: falseAnswers
+        false: falseAnswers,
+        fback: ret
     }
+
     callback(response);
 
+}
+
+function giveFeedback(answers) {
+
+    let feedback = [];
+    
+    feedback.push(answers.answer1);
+    feedback.push(answers.answer2);
+    feedback.push(answers.answer3);
+    feedback.push(answers.answer4);
+
+    return feedback;
 }
 
 function getExerciseAnswers(exerciseId) {
@@ -58,3 +81,4 @@ function getExerciseAnswers(exerciseId) {
 //Promise.resolve(promise).then(res => console.log(res));
 
 module.exports = checkExercise;
+exports = getExerciseAnswers;
