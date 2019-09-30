9530986d1a6bb285db6b898dadf951b114bb5541
Selamettin Dirik <selamettinn.95@gmail.com>
2019-09-30 18:42:35 +0300

    Signin reducer combined with the rootReducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux'
-import counter from './counter'
-import authentication from './authentication'
+import { combineReducers } from "redux";
+import counter from "./counter";
+import authentication from "./authentication";
+import signin from "./signin";
 
 const rootReducer = combineReducers({
-	counter,
-	authentication,
-})
+  counter,
+  authentication,
+  signin
+});
 
-export default rootReducer
+export default rootReducer;
