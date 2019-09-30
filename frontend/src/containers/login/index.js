cbda021975d15332274fbb3bcd96e7f2b9a358f3
Selamettin Dirik <selamettinn.95@gmail.com>
2019-09-30 18:40:09 +0300

    Login page is rearranged

--- frontend/src/containers/login/index.js
+++ frontend/src/containers/login/index.js
@@ -1,71 +1,74 @@
-import React, {Component} from 'react';
-import { Route, Link } from 'react-router-dom'
-import { Redirect } from 'react-router'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import Home from '../home'
-import {
-  login
-} from '../../redux/action-creators/authentication'
+import React, { Component } from "react";
+import { Redirect } from "react-router";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { login } from "../../redux/action-creators/authentication";
 
 class Login extends Component {
+  state = {
+    usernameOrEmail: "",
+    password: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    this.props.login(this.state.usernameOrEmail, this.state.password);
+  };
 
-	state = {
-			usernameOrEmail : "",
-			password : ""
-		};
-	handleChange = e => {
-		this.setState({
-			[e.target.id]: e.target.value
-		});
-	}
-	handleSubmit = e => {
-		this.props.login(this.state.usernameOrEmail, this.state.password);
-	}
-
-	render(){
-		if(this.props.loading) {
-			return (
-				<div>
-					<h1>LOADING</h1>
-				</div>
-			);
-		}
-		else if (this.props.status==0) {
-			return (
-				<Redirect to={{
-					pathname: "/",
-					state: {token: this.props.token}
-				}}/>
-			);
-		}
-		else {
-			return (
-				<div>
-					<form onSubmit={this.handleSubmit}>
-						<label>Username or Email:</label>
-						<input type="text" id="usernameOrEmail" onChange={this.handleChange}/>
-						<label>Password:</label>
-						<input type="password" id="password" onChange={this.handleChange}/>
-						<button>Login</button>
-					</form>
-				</div>
-			);
-		}
-	}
+  render() {
+    if (this.props.loading) {
+      return (
+        <div>
+          <h1>LOADING</h1>
+        </div>
+      );
+    } else if (this.props.status === 0) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/",
+            state: { token: this.props.token }
+          }}
+        />
+      );
+    } else {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>Username or Email:</label>
+            <input
+              type="text"
+              id="usernameOrEmail"
+              onChange={this.handleChange}
+            />
+            <label>Password:</label>
+            <input type="password" id="password" onChange={this.handleChange} />
+            <button>Login</button>
+          </form>
+        </div>
+      );
+    }
+  }
 }
 
 const mapStateToProps = ({ authentication }) => ({
   token: authentication.token,
   status: authentication.status,
-  loading: authentication.loading,
-})
+  loading: authentication.loading
+});
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  login,
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      login
+    },
+    dispatch
+  );
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Login)
+)(Login);
