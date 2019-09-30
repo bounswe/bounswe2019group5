aff96cb3004762720f4d71f6968bc9dc68b20b6e
Selamettin Dirik <selamettinn.95@gmail.com>
2019-09-30 18:39:15 +0300

    Home page is rearranged

--- frontend/src/containers/home/index.js
+++ frontend/src/containers/home/index.js
@@ -1,51 +1,64 @@
-import React from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import {
   increment,
   incrementAsync,
   decrement,
   decrementAsync
-} from '../../redux/action-creators/counter'
+} from "../../redux/action-creators/counter";
 
 const Home = props => (
   <div>
-
-  	<p>
-  		{props.location.state!=null && props.location.state!=undefined? props.location.state.token: "NO TOKEN"}
-	</p>
+    <p>
+      {props.location.state !== null && props.location.state !== undefined
+        ? props.location.state.token
+        : "NO TOKEN"}
+    </p>
 
     <h1>Home</h1>
     <p>Count: {props.count}</p>
 
     <p>
       <button onClick={props.increment}>Increment</button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
+        Increment Async
+      </button>
     </p>
 
     <p>
       <button onClick={props.decrement}>Decrementing</button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
+        Decrement Async
+      </button>
     </p>
 
-    <p><button onClick={() => props.history.push('/about-us')}>Go to about page via redux</button></p>
+    <p>
+      <button onClick={() => props.history.push("/about-us")}>
+        Go to about page via redux
+      </button>
+    </p>
   </div>
-)
+);
 
 const mapStateToProps = ({ counter }) => ({
   count: counter.count,
   isIncrementing: counter.isIncrementing,
   isDecrementing: counter.isDecrementing
-})
+});
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  increment,
-  incrementAsync,
-  decrement,
-  decrementAsync
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      increment,
+      incrementAsync,
+      decrement,
+      decrementAsync
+    },
+    dispatch
+  );
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Home)
+)(Home);
