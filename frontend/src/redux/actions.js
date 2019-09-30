08744f31997d285c319d91e24fe8cea3d6e3ae5d
Selamettin Dirik <selamettinn.95@gmail.com>
2019-09-30 18:41:01 +0300

    Signin is added to the actions

--- frontend/src/redux/actions.js
+++ frontend/src/redux/actions.js
@@ -1,7 +1,10 @@
-export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED'
-export const INCREMENT = 'counter/INCREMENT'
-export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED'
-export const DECREMENT = 'counter/DECREMENT'
+export const INCREMENT_REQUESTED = "counter/INCREMENT_REQUESTED";
+export const INCREMENT = "counter/INCREMENT";
+export const DECREMENT_REQUESTED = "counter/DECREMENT_REQUESTED";
+export const DECREMENT = "counter/DECREMENT";
 
-export const LOGIN_REQUESTED = 'login/LOGIN_REQUESTED'
-export const LOGIN = 'login/LOGIN'
+export const LOGIN_REQUESTED = "login/LOGIN_REQUESTED";
+export const LOGIN = "login/LOGIN";
+
+export const SIGNIN_REQUESTED = "signin/SIGNIN_REQUESTED";
+export const SIGNIN = "signin/SIGNIN";
