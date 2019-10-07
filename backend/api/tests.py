74c5be4fb23808625e5170dd78731dd7d916d0c7
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-10-07 17:47:45 +0300

    login bug fixed

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,42 +1,53 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+from .models import User
 
 class LoginViewTests(APITestCase):
-    def test_email_not_found(self):
-    
-        req = {
-            'email':'notFound@boun.com',
-            'password':'gHGG-jhj23'
+    def setUp(self):
+        data = {
+            'username': 'ada21',
+            'first_name': 'Ada',
+            'last_name': 'Lovelace',
+            'email': 'adaLovelace@email.com',
         }
-        response = self.client.post('/user/login',req)
-        
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
-    
-    def test_username_not_found(self):
-    
-        req = {
-            'user_name':'frederick_31',
-            'password':'gHGG-jhj23'
-        }
-        response = self.client.post('/user/login',req)
-        
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
-    
-    def test_email_not_valid(self):
-    
-        req = {
-            'email':'notValidEmailForm',
-            'password':'gHGG-jhj23'
-        }
-        response = self.client.post('/user/login',req)
-        
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
 
+        user = User(**data)
+        user.set_password('isa21-ad')
+        user.save()
+
+    def test_succesful_email(self):
+        req = {
+            'email_username':'adaLovelace@email.com',
+            'password':'isa21-ad'
+        }
+        response = self.client.post('/user/login',req)
+        self.assertEqual(response.status_code,status.HTTP_202_ACCEPTED)
+        self.assertTrue('token' in response.data.keys())
+
+    def test_succesful_username(self):
+        req = {
+            'email_username': 'ada21',
+            'password':'isa21-ad'
+        }
+        response = self.client.post('/user/login',req)
+        self.assertEqual(response.status_code,status.HTTP_202_ACCEPTED)
+        self.assertTrue('token' in response.data.keys())
+
+    def test_user_not_found(self):
+    
+        req = {
+            'email_username':'david',
+            'password':'gHGG-jhj23'
+        }
+        response = self.client.post('/user/login',req)
+        
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertTrue(not 'token' in response.data.keys())
+    
     def test_missing_field(self):
         req = {
-            'user_name':'frederick_31',
-            'email':'notFound@boun.com',
+            'email_username':'frederick_31',
             'password':'gHGG-jhj23'
         }
         for key in req:
@@ -44,10 +55,25 @@ class LoginViewTests(APITestCase):
             del deficient_req[key]
             response = self.client.post('/user/login',deficient_req)
             self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+            self.assertTrue(not 'token' in response.data.keys())
+        
+        response = self.client.post('/user/login',{})
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(not 'token' in response.data.keys())
 
-class RegisterViewTests(APITestCase):
-    def test_no_username(self):
-        pass
+    def test_wrong_password(self):
+        req = {
+            'email_username': 'ada21',
+            'password':'gHGG-jhj23'
+        }
+        response = self.client.post('/user/login',req)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+        self.assertTrue(not 'token' in response.data.keys())
+
+        
+
+#class RegisterViewTests(APITestCase):
+    
 class GuestViewTests(APITestCase):
     def test_is_token_valid(self):
         pass
--- backend/api/views.py
+++ backend/api/views.py
@@ -8,67 +8,80 @@ from rest_framework import status
 
 from .models import User
 
-class LoginView(APIView):
-    
-    def post(self,request):
-        if 'user_name' in request.data:
-            user = User.objects.filter(username=request.data.get('user_name')).first()
-        else:
-            user = User.objects.filter(email=request.data.get('email')).first()
 
-        if not user or not user.check_password(request.data.get('password')):
+class LoginView(APIView):
+
+    def post(self, request):
+        if 'email_username' in request.data and 'password' in request.data:
+            user = User.objects.filter(
+                username=request.data.get('email_username')).first()
+            if not user:
+                user = User.objects.filter(
+                    email=request.data.get('email_username')).first()
+
+            if not user:
+                return Response(
+                    {
+                        'message': 'user not found'
+                    },
+                    status=status.HTTP_404_NOT_FOUND)
+            else:
+                if not user.check_password(request.data.get('password')):
+                    return Response(
+                        {
+                            'message': 'password is wrong'
+                        },
+                        status=status.HTTP_401_UNAUTHORIZED)
+                else:
+                    token, created = Token.objects.get_or_create(user=user)
+                    return Response(
+                        {
+                            'token': token.key
+                        },
+                        status=status.HTTP_202_ACCEPTED)
+
+        else:
             return Response(
                 {
-                    'message':'user not found'
+                    'message': 'password is wrong'
                 },
-                status=status.HTTP_404_NOT_FOUND)
-        
-        token, created = Token.objects.get_or_create(user=user)
+                status=status.HTTP_400_BAD_REQUEST)
 
-        response = {
-            'token':token.key
-        }
-        
-        
-        return Response(response)
 
 class RegisterView(APIView):
-    def post(self,request):
+    def post(self, request):
         req = request.data
 
         try:
             user = User(
-                first_name = req['name'],
-                last_name = req['surname'],
-                username = req['user_name'],
-                email = req['email'],
-                nativeLanguage = req['native_language'])
+                first_name=req['name'],
+                last_name=req['surname'],
+                username=req['user_name'],
+                email=req['email'],
+                nativeLanguage=req['native_language'])
             user.set_password(req['password'])
             user.save()
 
             token, created = Token.objects.get_or_create(user=user)
 
             response = {
-                'token':token.key
+                'token': token.key
             }
 
-            return Response(response,status=status.HTTP_201_CREATED)
+            return Response(response, status=status.HTTP_201_CREATED)
         except:
             response = {
-                'message':'invalid registration attempt'
+                'message': 'invalid registration attempt'
             }
-            return Response(response,status=status.HTTP_400_BAD_REQUEST)
-            
-        
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GuestView(APIView):
-    def post(self,request):
+    def post(self, request):
         permission_classes = (IsAuthenticated, )
 
-        #check that registeredUser cannot request as a Guest
+        # check that registeredUser cannot request as a Guest
         if not request.user in User.objects.all():
-            return Response({},status=status.HTTP_200_OK)
+            return Response({}, status=status.HTTP_200_OK)
         else:
-            return Response({},status=status.HTTP_401_UNAUTHORIZED)
-        
+            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
