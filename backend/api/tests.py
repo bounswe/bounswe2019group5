b5a5b22fbf13c26768d8e925757709dabf24b977
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-10-07 12:52:43 +0300

    login tests added

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,12 +1,49 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from rest_framework import status
 
 class LoginViewTests(APITestCase):
     def test_email_not_found(self):
     
-        response = self.client.post('/user/login',{})
-        self.assertEqual(4, 4)
+        req = {
+            'email':'notFound@boun.com',
+            'password':'gHGG-jhj23'
+        }
+        response = self.client.post('/user/login',req)
         
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+    
+    def test_username_not_found(self):
+    
+        req = {
+            'user_name':'frederick_31',
+            'password':'gHGG-jhj23'
+        }
+        response = self.client.post('/user/login',req)
+        
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+    
+    def test_email_not_valid(self):
+    
+        req = {
+            'email':'notValidEmailForm',
+            'password':'gHGG-jhj23'
+        }
+        response = self.client.post('/user/login',req)
+        
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+
+    def test_missing_field(self):
+        req = {
+            'user_name':'frederick_31',
+            'email':'notFound@boun.com',
+            'password':'gHGG-jhj23'
+        }
+        for key in req:
+            deficient_req = {**req}
+            del deficient_req[key]
+            response = self.client.post('/user/login',deficient_req)
+            self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
 
 class RegisterViewTests(APITestCase):
     def test_no_username(self):
