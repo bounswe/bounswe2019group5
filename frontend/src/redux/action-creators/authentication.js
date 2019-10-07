74bf601c3d77fbeaf3b2fac4f2cd009f6a0f79a2
Selamettin Dirik <selamettinn.95@gmail.com>
2019-10-07 16:42:10 +0300

    SignIn page is converted into the SignUp page

--- frontend/src/redux/action-creators/authentication.js
+++ frontend/src/redux/action-creators/authentication.js
@@ -1,51 +1,45 @@
-import {
-	LOGIN_REQUESTED,
-	LOGIN,
-	SIGNIN_REQUESTED,
-	SIGNIN,
-} from '../actions';
+import { LOGIN_REQUESTED, LOGIN, SIGNUP_REQUESTED, SIGNUP } from "../actions";
 
-import {login as login_api} from '../../api/authentication';
-import {signin as signin_api} from '../../api/authentication';
+import { login as login_api } from "../../api/authentication";
+import { signup as signup_api } from "../../api/authentication";
 
 export const login = (usernameOrEmail, password) => {
-	return dispatch => {
-		dispatch({
-			type : LOGIN_REQUESTED
-		});
+  return dispatch => {
+    dispatch({
+      type: LOGIN_REQUESTED
+    });
 
-		login_api(usernameOrEmail, password)
-			.then(response => {
-				dispatch({
-					type : LOGIN,
-					token : response.token,
-					status : response.status,
-				});
-			});
-	}
-}
-
-export const signin = (
-	name,
-	surname,
-	email,
-	user_name,
-	password,
-	native_language
-  ) => {
-	return dispatch => {
-	  dispatch({
-		type: SIGNIN_REQUESTED
-	  });
-  
-	  signin_api(name, surname, email, user_name, password, native_language).then(
-		response => {
-		  dispatch({
-			type: SIGNIN,
-			token: response.token,
-			status: response.status
-		  });
-		}
-	  );
-	};
+    login_api(usernameOrEmail, password).then(response => {
+      dispatch({
+        type: LOGIN,
+        token: response.token,
+        status: response.status
+      });
+    });
   };
+};
+
+export const signup = (
+  name,
+  surname,
+  email,
+  user_name,
+  password,
+  native_language
+) => {
+  return dispatch => {
+    dispatch({
+      type: SIGNUP_REQUESTED
+    });
+
+    signup_api(name, surname, email, user_name, password, native_language).then(
+      response => {
+        dispatch({
+          type: SIGNUP,
+          token: response.token,
+          status: response.status
+        });
+      }
+    );
+  };
+};
