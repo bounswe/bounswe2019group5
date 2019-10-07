0a7c6862b7a22ca0e8919aec933e6965fecb9d64
Selamettin Dirik <selamettinn.95@gmail.com>
2019-10-07 16:55:48 +0300

    SignIn page is converted into the SignUp page

--- frontend/src/api/authentication/index.js
+++ frontend/src/api/authentication/index.js
@@ -1,41 +1,35 @@
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const login = async (
-    usernameOrEmail, 
-    password
+export const login = async (usernameOrEmail, password) => {
+  await timeout(3000);
+  if (usernameOrEmail === "user" && password === "12345678") {
+    return { token: "this is a token", status: 0 };
+  } else {
+    return { token: null, status: -1 };
+  }
+};
+
+export const signup = async (
+  name,
+  surname,
+  email,
+  user_name,
+  password,
+  native_language
 ) => {
-	await timeout(3000);
-	if (usernameOrEmail==="user" && password==="12345678") {
-		return {token: "this is a token", status: 0};
-	}
-	else {
-		return {token: null, status: -1};
-	}
-}
-
-export const signin = async (
-    name,
-    surname,
-    email,
-    user_name,
-    password,
-    native_language
-  ) => {
-
-    await timeout(3000);
-    if (
-      name === "Mert" &&
-      surname === "Maysallar" &&
-      email === "mmaysallar@gmail.com" &&
-      user_name === "MM" &&
-      password === "12345678" &&
-      native_language === "Turkish"
-    ) {
-      return { token: "this is a signed user token", status: 0 };
-    } else {
-      return { token: null, status: -1 };
-    }
-    
-  };
+  await timeout(3000);
+  if (
+    name === "mert" &&
+    surname === "maysallar" &&
+    email === "mmaysallar@gmail.com" &&
+    user_name === "mm" &&
+    password === "12345678" &&
+    native_language === "turkish"
+  ) {
+    return { token: "this is a signed up user token", status: 0 };
+  } else {
+    return { token: null, status: -1 };
+  }
+};
