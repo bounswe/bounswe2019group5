43fb423c80eff9dced5e3a2e0abd730f693cd8dc
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-10-08 20:43:05 +0300

    Added some dummy questions to the Prof exam page

--- languageLearning/app/src/main/java/com/example/languageLearning/ProfExamActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/ProfExamActivity.java
@@ -11,14 +11,33 @@ public class ProfExamActivity extends AppCompatActivity {
 
     TextView question_textview;
     Button buttons[] = new Button[4];
+    int currentQuestionIndex;
 
-    final private String dummy_question = "Which of the options below is a correct sentence?";
+    final private String[] dummy_questions = {
+            "Which of the options below is a correct sentence?",
+            "Choose the odd one out",
+            "Which is the indirect speech for the sentence \"He said, 'I love this place' \""
+    };
 
-    final private String[] dummy_options = {
-            "Man the walked up the street",
-            "The man walk up the street",
-            "The man walked up the street",
-            "The man walked up a street"
+    final private String[][] dummy_options = {
+            new String[]{
+                    "Man the walked up the street",
+                    "The man walk up the street",
+                    "The man walked up the street",
+                    "The man walked up a street"
+            },
+            new String[]{
+                    "shoe",
+                    "orange",
+                    "pineapple",
+                    "apple"
+            },
+            new String[]{
+                    "He said that I love this place",
+                    "He said that he loved that place",
+                    "He said that I loved that place",
+                    "He said that he loves this place"
+            }
     };
 
     @Override
@@ -30,8 +49,31 @@ public class ProfExamActivity extends AppCompatActivity {
         buttons[1] = findViewById(R.id.prof_option_2);
         buttons[2] = findViewById(R.id.prof_option_3);
         buttons[3] = findViewById(R.id.prof_option_4);
-        question_textview.setText(dummy_question);
         for (int i=0; i<4; i++)
-            buttons[i].setText(dummy_options[i]);
+            buttons[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    OptionClicked((Button)v);
+                }
+            });
+        currentQuestionIndex = 0;
+        setCurrentQuestion(currentQuestionIndex);
+    }
+
+    private void setCurrentQuestion(int index) {
+        question_textview.setText(dummy_questions[index]);
+        for (int i=0; i<4; i++)
+            buttons[i].setText(dummy_options[index][i]);
+    }
+
+    private void OptionClicked(Button v) {
+        currentQuestionIndex += 1;
+
+        if (currentQuestionIndex == dummy_questions.length) {
+            finish();
+            return ;
+        }
+
+        setCurrentQuestion(currentQuestionIndex);
     }
 }
