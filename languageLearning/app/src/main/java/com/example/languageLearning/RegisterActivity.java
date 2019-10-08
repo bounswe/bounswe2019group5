c0c8fcd76900aaf02efd89c6723019e84973df2d
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-10-08 20:01:45 +0300

    LoginActivity and RegisterActivity now acquires a handle to MyApplication in onCreate

--- languageLearning/app/src/main/java/com/example/languageLearning/RegisterActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/RegisterActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private MyApplication app;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        app = (MyApplication)getApplication();
         setContentView(R.layout.activity_register);
     }
 }
