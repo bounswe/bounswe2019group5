aabc672035ac5cb65824d8600be5405bf1b58bbd
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-10-09 17:56:57 +0300

    test cases added for /proficiency call

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -4,6 +4,62 @@ from rest_framework import status
 from .models import User
 
 
+class ProficiencyTest(APITestCase):
+    def setUp(self):
+        q = {
+            "questionLevel": "A2",
+            "questionType": "vocab",
+            "questionId": "asd12ejd",
+            "questionAnswer": "bring",
+            "questionText": "to take or carry someone or \
+                something to a place or a person, or \
+                in the direction of the person speaking",
+            "questionOptions": [
+                "bring",
+                "catch",
+                "follow",
+                "burn"
+            ],
+        }
+        question = Question.objects.create(**q)
+
+        self.req = {
+            "testLanguage": "english",
+        }
+
+    def test_number_of_question(self):
+        
+        response = self.client.post('/profiency', self.req)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get('testQuestions')), 6)
+
+    def test_number_of_question_options(self):
+        
+        response = self.client.post('/profiency', self.req)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        numberOfQuestions = len(response.data.get('testQuestions'))
+        for i in range(numberOfQuestions):
+            self.assertEqual(
+                len(response.data.get('testQuestions')[i].get('questionOptions')),
+                4)
+
+    def test_answer_in_options(self):
+
+        response = self.client.post('/profiency', self.req)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        numberOfQuestions = len(response.data.get('testQuestions'))
+        for i in range(numberOfQuestions):
+            options = response.data.get('testQuestions')[i].get('questionOptions')
+            answer = response.data.get('testQuestions')[i].get('questionAnswer')
+            self.assertTrue(answer in options)
+            options.remove(answer)
+            self.assertTrue(not answer in options)
+  
+
+
+
 class LoginViewTests(APITestCase):
     def setUp(self):
         data = {
@@ -12,7 +68,6 @@ class LoginViewTests(APITestCase):
             'last_name': 'Lovelace',
             'email': 'adaLovelace@email.com',
         }
-
         user = User(**data)
         user.set_password('isa21-ad')
         user.save()
