f635e6cc8eea17ce97c5c7380cfd0816564a8eca
Yusuf Mert Bila <mert.bila@gmail.com>
2019-10-10 18:00:36 +0300

    added cors support and removed unnecessary docker containers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,10 +15,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ORIGIN_ALLOW_ALL=True
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -31,6 +32,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
