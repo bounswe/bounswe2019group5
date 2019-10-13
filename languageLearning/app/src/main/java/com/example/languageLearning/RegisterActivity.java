4c2eb7e195195b3c3386640c8b58f93509058d49
ali-deser-2016400261 <meric.deser@boun.edu.tr>
2019-10-14 00:08:51 +0300

    SignUp Page Activity Update

--- languageLearning/app/src/main/java/com/example/languageLearning/RegisterActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/RegisterActivity.java
@@ -2,16 +2,28 @@ package com.example.languageLearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private MyApplication app;
+    EditText name, surname, email;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         app = (MyApplication)getApplication();
         setContentView(R.layout.activity_register);
+
+        name = findViewById(R.id.name);
+        surname = findViewById(R.id.surname);
+        email =  findViewById(R.id.email);
     }
 }
