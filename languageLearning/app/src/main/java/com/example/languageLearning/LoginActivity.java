0c13f3c80b0a44838559d67c04ec31b3cf30a8a5
Mahmut <mahmutuzunpostalci@gmail.com>
2019-10-16 23:10:26 +0300

    Connection to backend has been created

--- languageLearning/app/src/main/java/com/example/languageLearning/LoginActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.languageLearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
@@ -11,38 +12,130 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private final String TAG = LoginActivity.class.getName();
+    private final String TAG = "TEST";
+
+    String token;
 
     TextView registerText;
 
     Button loginButton;
     private MyApplication app;
 
+    Dialog popup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         app = (MyApplication)getApplication();
         setContentView(R.layout.activity_main);
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
         loginButton = findViewById(R.id.loginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText username, password;
-                username = findViewById(R.id.editText);
-                password = findViewById(R.id.editText2);
-                String susername = username.getText().toString();
-                String spassword = password.getText().toString();
-                Log.d(TAG, "UN:"+susername + ", P:" + spassword);
+                EditText usernameView, passwordView;
+                usernameView = findViewById(R.id.usernameView);
+                passwordView = findViewById(R.id.passwordView);
+                String username = usernameView.getText().toString();
+                String password = passwordView.getText().toString();
+                Log.d(TAG, "UN:"+username + ", P:" + password);
 
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
+                if(username.isEmpty()){
+                    showErrorPopup("You must enter an e-mail/username");
+                }else if(password.isEmpty()){
+                    showErrorPopup("You must enter a password");
+                }
+
+                if(!(username.isEmpty() | password.isEmpty())) {
+                    String returnedToken;
+                    returnedToken = sendLoginRequest(username, password, requestQueue);
+                    if(!returnedToken.isEmpty()) {
+                        app.setToken(returnedToken);
+                    }
+                }
+                //Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                //startActivity(intent);
             }
         });
 
         registerText = findViewById(R.id.regısterText);
         registerText.setPaintFlags(registerText.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
 
+
+    }
+
+    public void showErrorPopup(final String errorMessage){
+        popup = new Dialog(this);
+        TextView errorMessageView;
+        Button closePopupButton;
+        popup.setContentView(R.layout.login_error_popup);
+        errorMessageView = popup.findViewById(R.id.errorMessageView);
+        closePopupButton = popup.findViewById(R.id.closePopupButton);
+        errorMessageView.setText(errorMessage);
+        closePopupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popup.dismiss();
+            }
+        });
+        popup.show();
+    }
+
+    public String sendLoginRequest(String username, String password, RequestQueue reqQ){
+        String loginUrl = "http://18.197.149.174:8000/user/login";
+
+        JSONObject json = new JSONObject();
+        try{
+            json.put("email_username", username);
+            json.put("password",password);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, loginUrl, json, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "Response: " + response.toString());
+                token = response.toString();
+                showErrorPopup(response.toString());
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Response: " + error.toString());
+                if(error.toString().equals("com.android.volley.AuthFailureError")){
+                    showErrorPopup("The password you entered is incorrect");
+                }else if(error.toString().equals("com.android.volley.ClientError")){
+                    showErrorPopup("The username you entered does not exist");
+                }
+            }
+        });
+
+        reqQ.add(jsonRequest);
+
+        return token;
     }
 }
--- languageLearning/app/src/main/java/com/example/languageLearning/MyApplication.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/MyApplication.java
@@ -3,15 +3,24 @@ package com.example.languageLearning;
 import android.app.Application;
 
 public class MyApplication extends Application {
+    private String token;
 
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
+
     @Override
     public void onTerminate() {
         super.onTerminate();
     }
-    
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
