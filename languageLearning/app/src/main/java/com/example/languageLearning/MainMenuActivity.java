7e4f543ade727a33c019e4046f675af30873f9b5
Mahmut <mahmutuzunpostalci@gmail.com>
2019-10-17 19:03:04 +0300

    MainMenuActivity updated

--- languageLearning/app/src/main/java/com/example/languageLearning/MainMenuActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/MainMenuActivity.java
@@ -2,17 +2,72 @@ package com.example.languageLearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
     private final String TAG = "TEST";
     private MyApplication app;
+    TextView welcomeMessage;
+    ImageButton profileButton, logoutButton;
+    Dialog popup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         app = (MyApplication)getApplication();
 
+        welcomeMessage = findViewById(R.id.welcomeMessage);
+        profileButton = findViewById(R.id.profileButton);
+        logoutButton = findViewById(R.id.logoutButton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, ProfExamActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLogoutPopup();
+            }
+        });
+
+        Bundle b = getIntent().getExtras();
+        String username = b.getString("USERNAME");
+        welcomeMessage.setText("Hello " + username + "!");
+
+    }
+
+    public void showLogoutPopup(){
+        popup = new Dialog(this);
+        Button logoutYesButton, logoutNoButton;
+        popup.setContentView(R.layout.logout_popup);
+        logoutYesButton = popup.findViewById(R.id.logoutYesButton);
+        logoutYesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                app.setToken("");
+                Intent intent = new Intent(MainMenuActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        logoutNoButton = popup.findViewById(R.id.logoutNoButton);
+        logoutNoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popup.dismiss();
+            }
+        });
+        popup.show();
     }
 }
