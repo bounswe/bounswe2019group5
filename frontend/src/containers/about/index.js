999ba5a58da75a4fd68518c83d5f2ab1c3765a0a
unknown <meltemsuicmez1@gmail.com>
2019-10-18 23:09:44 +0300

    removed redundant imports

--- frontend/src/containers/about/index.js
+++ frontend/src/containers/about/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
