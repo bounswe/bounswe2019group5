64fd991ad39780851c3e5b56063048109350af9e
Selamettin Dirik <selamettinn.95@gmail.com>
2019-10-19 16:23:55 +0300

    css styles are added to the tests

--- frontend/src/containers/testResult/index.js
+++ frontend/src/containers/testResult/index.js
@@ -1,11 +1,118 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
-import "./testResult.css";
+import Avatar from "@material-ui/core/Avatar";
+//import Button from "@material-ui/core/Button";
+import { Button } from "react-bootstrap";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { login } from "../../redux/action-creators/authentication";
+import styles from "./styles";
+import { withStyles } from "@material-ui/core/styles";
+import { green, purple } from "@material-ui/core/colors";
 
 const TestResult = props => {
+  const { classes } = props;
   if (props.authentication.token != null) {
     return (
+      <Container
+        component="main"
+        maxWidth="xs"
+        justify="center"
+        alignItems="center"
+      >
+        <CssBaseline />
+        <Grid item component={Paper} square>
+          <div className={classes.paper}>
+            <div className="d-flex flex-column">
+              <Typography component="h1" variant="h3" align="center">
+                Congratulations!
+              </Typography>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                variant="info"
+                className={classes.submit}
+                onClick={() => {}}
+              >
+                {props.test !== null
+                  ? props.test.testResult !== null
+                    ? "Number Of True Answers: " +
+                      props.test.testResult.nuOfTrueAnswers
+                    : ""
+                  : "isFinished not working"}
+              </Button>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                variant="info"
+                className={classes.submit}
+                onClick={() => {}}
+              >
+                {props.test !== null
+                  ? props.test.testResult !== null
+                    ? "Number Of False Answers: " +
+                      props.test.testResult.nuOfFalseAnswers
+                    : ""
+                  : "isFinished not working"}
+              </Button>
+
+              <Button
+                size="lg"
+                type="submit"
+                fullWidth
+                variant="contained"
+                variant="warning"
+                className={classes.submit}
+                onClick={() => {}}
+              >
+                {props.test !== null
+                  ? props.test.testResult !== null
+                    ? "Your Level: " +
+                      ((props.test.testResult.nuOfTrueAnswers * 100) /
+                        props.test.testResult.nuOfQuestions ===
+                      20
+                        ? "A2"
+                        : (props.test.testResult.nuOfTrueAnswers * 100) /
+                            props.test.testResult.nuOfQuestions ===
+                          40
+                        ? "B1"
+                        : (props.test.testResult.nuOfTrueAnswers * 100) /
+                            props.test.testResult.nuOfQuestions ===
+                          60
+                        ? "B2"
+                        : (props.test.testResult.nuOfTrueAnswers * 100) /
+                            props.test.testResult.nuOfQuestions ===
+                          80
+                        ? "C1"
+                        : "C2")
+                    : ""
+                  : "props.test is not working"}
+              </Button>
+
+              <Button
+                variant="contained"
+                fullWidth
+                variant="secondary"
+                onClick={() => props.history.push("/home")}
+              >
+                Return To Home Page!
+              </Button>
+            </div>
+          </div>
+        </Grid>
+      </Container>
+
+      /*
       <div className="card">
         <p className="container">
           {props.test !== null
@@ -52,7 +159,7 @@ const TestResult = props => {
         <button className="button" onClick={() => props.history.push("/home")}>
           Return To Home Page!
         </button>
-      </div>
+      </div>*/
     );
   } else {
     return (
@@ -70,4 +177,4 @@ const mapStateToProps = ({ test, authentication }) => ({
   authentication
 });
 
-export default connect(mapStateToProps)(TestResult);
+export default withStyles(styles)(connect(mapStateToProps)(TestResult));
