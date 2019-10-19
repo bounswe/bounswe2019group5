1fb1c79df3102ab0e2d46d322e8037db4eb6981d
Selamettin Dirik <selamettinn.95@gmail.com>
2019-10-19 13:08:11 +0300

    app.js is updated

--- frontend/src/containers/app/index.js
+++ frontend/src/containers/app/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
@@ -7,62 +7,67 @@ import About from "../about";
 import Login from "../login";
 import SignUp from "../signup";
 import ProfTest from "../profTest";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Form, Container, Row, Col, Image, Button, ButtonToolbar } from 'react-bootstrap';
+import TestResult from "../testResult";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  Form,
+  Container,
+  Row,
+  Col,
+  Image,
+  Button,
+  ButtonToolbar
+} from "react-bootstrap";
 import { logout } from "../../redux/action-creators/authentication";
 
 class App extends Component {
-
   render() {
     return (
       <div>
-        <style type="text/css">    {`
+        <style type="text/css">
+          {" "}
+          {`
             .bg-white {
               background-color: white;
               color: blue;
-            }   `
-          }
-        </style> 
+            }   `}
+        </style>
         <Navbar className="bg-white justify-content-between">
           <Navbar.Brand href="#home">
             <Link to="/">
-              <Image  width={250} 
-                      height={100}
-                      alt="bonibon"
-                      src="https://github.com/bounswe/bounswe2019group5/raw/master/Images/logo.jpeg"
-                      fluid />
+              <Image
+                width={250}
+                height={100}
+                alt="bonibon"
+                src="https://github.com/bounswe/bounswe2019group5/raw/master/Images/logo.jpeg"
+                fluid
+              />
             </Link>
           </Navbar.Brand>
           <Nav className="mr-auto">
-            {this.props.authentication.token &&
-              (
-                <Link to="/login" onClick={() => this.props.logout()}>
-                  <Button variant="outline-warning">Logout</Button>
-                </Link>
-              )
-            }
-            {!this.props.authentication.token &&
-              (
-                
-                <Link to="/login">
-                  <Button variant="outline-warning">Login</Button>
-                </Link>
-                
-              )
-            }
-            {!this.props.authentication.token &&
-              (
-                <Link to="/signup">
-                  <Button variant="outline-success">Sign-up</Button>
-                </Link>
-              )
-            }
+            {this.props.authentication.token && (
+              <Link to="/login" onClick={() => this.props.logout()}>
+                <Button variant="outline-warning">Logout</Button>
+              </Link>
+            )}
+            {!this.props.authentication.token && (
+              <Link to="/login">
+                <Button variant="outline-warning">Login</Button>
+              </Link>
+            )}
+            {!this.props.authentication.token && (
+              <Link to="/signup">
+                <Button variant="outline-success">Sign-up</Button>
+              </Link>
+            )}
           </Nav>
           <Form inline>
             <Nav.Link href="about-us">
               <Link to="/about-us">
                 <Button variant="outline-info">About</Button>
-              </Link> 
+              </Link>
             </Nav.Link>
           </Form>
         </Navbar>
@@ -73,9 +78,10 @@ class App extends Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/prof-test" component={ProfTest} />
+          <Route exact path="/test-result" component={TestResult} />
         </main>
       </div>
-    )
+    );
   }
 }
 
