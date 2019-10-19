5d6b1680d7ce91e13dd3c16e98321667ca70ebbf
tony <hasan.ozturk@boun.edu.tr>
2019-10-19 17:14:44 +0300

    Created a basic bridge activity

---
+++ languageLearning/app/src/main/java/com/example/languageLearning/BridgeActivity.java
@@ -0,0 +1,58 @@
+package com.example.languageLearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class BridgeActivity extends AppCompatActivity {
+
+    Intent i = new Intent(this, ProfExamActivity.class);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //remove title bar
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
+        setContentView(R.layout.activity_bridge);
+    }
+
+    public void onClickTurkish(View view){
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+        i.putExtra("languageChoice", buttonText);
+        startActivity(i);
+
+    }
+
+    public void onClickEnglish(View view){
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+        i.putExtra("languageChoice", buttonText);
+        startActivity(i);
+
+    }
+
+    public void onClickGerman(View view){
+
+        Button b = (Button) view;
+        String buttonText = b.getText().toString();
+        i.putExtra("languageChoice", buttonText);
+        startActivity(i);
+
+    }
+
+}
