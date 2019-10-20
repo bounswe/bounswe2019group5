855a65559ab32a10e38c1fb31f5706737c3b4048
Mahmut <mahmutuzunpostalci@gmail.com>
2019-10-20 13:33:38 +0300

    Connections between activities has benn made.

--- languageLearning/app/src/main/java/com/example/languageLearning/ProfResultActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/ProfResultActivity.java
@@ -2,13 +2,18 @@ package com.example.languageLearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ProfResultActivity extends AppCompatActivity {
 
     TextView correctAnswer, incorrectAnswer, level;
+    ImageButton homeButton;
     private static final String TAG = "MyActivity";
 
     @Override
@@ -19,6 +24,15 @@ public class ProfResultActivity extends AppCompatActivity {
         correctAnswer = findViewById(R.id.correctAnswer);
         incorrectAnswer = findViewById(R.id.incorrectAnswer);
         level = findViewById(R.id.level);
+        homeButton = findViewById(R.id.homeButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfResultActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Bundle b = getIntent().getExtras();
         int correctAnswerCount = b.getInt("COUNT_CORRECT");
