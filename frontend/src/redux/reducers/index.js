bd4463a36a5ba44694328ed1657eb5237f08ece3
Selamettin Dirik <selamettinn.95@gmail.com>
2019-10-25 00:57:06 +0300

    vocabulary exercise reducer is added

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -3,13 +3,14 @@ import counter from "./counter";
 import authentication from "./authentication";
 import test from "./test";
 import userInfo from "./userInfo";
-
+import exercises from "./exercises";
 
 const rootReducer = combineReducers({
   counter,
   authentication,
   test,
   userInfo,
+  exercises
 });
 
 export default rootReducer;
