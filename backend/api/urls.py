d0a3d0cd2cbb38db841dd2e8824b2e91cb804980
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-10-25 23:32:55 +0300

    url configuration for api documentation

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,31 @@
 from django.urls import path
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+
 from . import views
 
-app_name = 'login'
 urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
     path('register', views.RegisterView.as_view(), name='register'),
     path('login/guest', views.GuestView.as_view(), name='guest'),
     path('proficiency', views.ProficiencyView.as_view(), name='prof'),
     path('profile', views.ProfileView.as_view(), name='profile'),
+
+
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='docs'),
+
+    path('redocs/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='redocs'),
+
+    path('openapi/', get_schema_view(
+        title="Bonibon",
+        description="Bonibon API Schema",
+    ), name='openapi-schema')
+
+
 ]
