7f7513a480321692d45e6d80d15177d53a53139d
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-11-03 21:23:46 +0300

    /search/ endpoint added

---
+++ backend/api/filters/search.py
@@ -0,0 +1,19 @@
+from django_filters import rest_framework as filters
+from django.db.models import Q
+
+
+class SearchFilterSet(filters.FilterSet):
+
+    tag = filters.CharFilter(method='filter_tag',
+                             label='search exercise with tag')
+    keyword = filters.CharFilter(method='filter_keyword')
+
+    @staticmethod
+    def filter_tag(queryset, name, value):
+        tagged = Q(tags__contains=[value])
+        return queryset.filter(tagged)
+
+    @staticmethod
+    def filter_keyword(queryset, name, value):
+        keyword = Q(keywords__contains=[value])
+        return queryset.filter(keyword)
