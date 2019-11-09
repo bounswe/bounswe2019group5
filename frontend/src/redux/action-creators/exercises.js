65160fbe67bcc11cf4d3974e9e3096d461660c70
Selamettin Dirik <selamettinn.95@gmail.com>
2019-11-09 18:08:37 +0300

    recently-unnecessary parts are removed

--- frontend/src/redux/action-creators/exercises.js
+++ frontend/src/redux/action-creators/exercises.js
@@ -1,12 +1,6 @@
-import {
-  VOCABULARY_TEST_REQUESTED,
-  VOCABULARY_TEST,
-  TEST_RESULT_REQUESTED,
-  TEST_RESULT
-} from "../actions";
+import { VOCABULARY_TEST_REQUESTED, VOCABULARY_TEST } from "../actions";
 
 import { get_vocabulary_test as get_vocabulary_test_api } from "../../api/exercises";
-import { get_test_result as get_test_result_api } from "../../api/test";
 
 export const get_vocabulary_test = (token, selectedLanguage) => {
   return dispatch => {
@@ -21,19 +15,3 @@ export const get_vocabulary_test = (token, selectedLanguage) => {
     });
   };
 };
-/*
-export const get_test_result = (token, testId, answers) => {
-  return dispatch => {
-    dispatch({
-      type: TEST_RESULT_REQUESTED
-    });
-
-    get_test_result_api(token, testId, answers).then(response => {
-      dispatch({
-        type: TEST_RESULT,
-        testResult: response
-      });
-    });
-  };
-};
-*/
