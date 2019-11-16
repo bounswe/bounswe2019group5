77bc630fbf0a60c3496c71424bdb22c7becf3127
Yusuf Mert Bila <mert.bila@gmail.com>
2019-11-16 15:03:05 +0300

    added reducers and actions for recommendation

--- frontend/src/containers/recommendation/index.js
+++ frontend/src/containers/recommendation/index.js
@@ -1,26 +1,17 @@
-import React, { Component } from "react";
-import { Redirect, browserHistory } from "react-router";
+import React, { Component } from 'react';
+import { Redirect } from "react-router";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { login, clear_authentication } from '../../redux/action-creators/authentication';
-import { clear_user_profile } from '../../redux/action-creators/userInfo';
-import { withStyles } from '@material-ui/core/styles';
-import { Select } from "@material-ui/core";
+
+import { Button} from 'react-chat-elements';
+
+import { get_user_recommendations, set_recommendation_language } from '../../redux/action-creators/recommendation';
 
 
 class Recommendation extends Component {
 
-    state = {
-        language: "english",
-        recommended_users: []
+    handleChange = e => {
+        this.props.set_recommendation_language(e.target.value);
     };
 
     render() {
@@ -29,24 +20,47 @@ class Recommendation extends Component {
             <h3>Recommendations</h3>
             <select
                 className="input form-control"
-                
-                value={this.state.language}>
+                onChange={this.handleChange}
+                value={this.props.recommendation.language}>
                 <option value=''></option>
                 <option value='english'>English</option>
                 <option value='german'>German</option>
                 <option value='turkish'>Turkish</option>
             </select>
+            <br></br>
+            <Button
+                color='blue'
+                backgroundColor='green'
+                text='Get Recommendations'
+                onClick={
+                    () => {
+                        this.props.get_user_recommendations(this.props.userInfo.token, this.props.recommendation.language);
+                }}
+            />
         </div>
-        
-        
-        
-        
-        
-        
         
         )
     }
 
 }
 
-export default Recommendation;
+const mapStateToProps = ({ userInfo, recommendation }) => ({
+    userInfo,
+    recommendation,
+  });
+  
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(
+        {
+            get_user_recommendations,
+            set_recommendation_language
+        },
+        dispatch
+    );
+
+    export default (
+        connect(
+          mapStateToProps,
+          mapDispatchToProps
+        )(Recommendation)
+    );
