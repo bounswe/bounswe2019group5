b7f24a89c354c2288b633c6b2ff901832d9495d2
Selamettin Dirik <selamettinn.95@gmail.com>
2019-11-16 23:24:40 +0300

    grammar_test action-creator is created

--- frontend/src/redux/action-creators/exercises.js
+++ frontend/src/redux/action-creators/exercises.js
@@ -1,6 +1,12 @@
-import { VOCABULARY_TEST_REQUESTED, VOCABULARY_TEST } from "../actions";
+import {
+  VOCABULARY_TEST_REQUESTED,
+  VOCABULARY_TEST,
+  GRAMMAR_TEST_REQUESTED,
+  GRAMMAR_TEST
+} from "../actions";
 
 import { get_vocabulary_test as get_vocabulary_test_api } from "../../api/exercises";
+import { get_grammar_test as get_grammar_test_api } from "../../api/exercises";
 
 export const get_vocabulary_test = (token, selectedLanguage) => {
   return dispatch => {
@@ -11,6 +17,20 @@ export const get_vocabulary_test = (token, selectedLanguage) => {
       dispatch({
         type: VOCABULARY_TEST,
         vocabularyTest: response
+      });
+    });
+  };
+};
+
+export const get_grammar_test = (token, selectedLanguage) => {
+  return dispatch => {
+    dispatch({
+      type: GRAMMAR_TEST_REQUESTED
+    });
+    get_grammar_test_api(token, selectedLanguage).then(response => {
+      dispatch({
+        type: GRAMMAR_TEST,
+        grammarTest: response
       });
     });
   };
