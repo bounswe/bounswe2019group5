f77a765fbf2b209bde96e5a742712a66f06d4ebd
unknown <meltemsuicmez1@gmail.com>
2019-11-18 22:00:54 +0300

    added ratings&other user profiles

--- frontend/src/containers/profile/index.js
+++ frontend/src/containers/profile/index.js
@@ -2,28 +2,45 @@ import React, { Component } from "react";
 import { Redirect } from "react-router";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { set_user_profile } from "../../redux/action-creators/userInfo";
+import {
+  set_user_profile,
+  set_other_user_profile
+} from "../../redux/action-creators/userInfo";
 import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 import LangTab from "./langTab";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Paper from "@material-ui/core/Paper";
+import Ratings from "./ratings";
 
 class Profile extends Component {
+  state = { selfProfile: true };
   constructor(props) {
     super(props);
     console.log(props.userInfo.token);
   }
   componentDidMount() {
-    if (this.props.userInfo.userProfile)
-      this.props.set_user_profile(this.props.userInfo.token);
-    console.log(this.props.userInfo.userProfile.attended_languages);
-  }
+    if (
+      this.props.match.params.user !=
+      ":" + this.props.userInfo.userProfile.username
+    ) {
+      this.props.set_other_user_profile(
+        this.props.userInfo.token,
+        this.props.match.params.user.substring(1)
+      );
+      this.setState({ selfProfile: false });
+      console.log("other profile");
+      console.log("SELF PROFILE" + this.state.selfProfile);
 
+    } else  {
+      this.props.set_user_profile(this.props.userInfo.token);
+      console.log("self profile");
+    }
+  }
   render() {
     const { classes } = this.props;
     if (this.props.userInfo.token == null) {
@@ -40,19 +57,38 @@ class Profile extends Component {
           <CssBaseline />
           <Grid xs={3} className={classes.paper}>
             <div className={classes.paper}>
-              <Card border="warning" className="text-center" style={{ width: "18rem" }}>
+              <Card
+                border="warning"
+                className="text-center"
+                style={{ width: "18rem" }}
+              >
                 <Card.Header>
                   <AccountCircleIcon fontSize="large"> </AccountCircleIcon>
                 </Card.Header>
                 <Card.Body>
-                  <Card.Title>
-                    {this.props.userInfo.userProfile.username}
-                  </Card.Title>
+                  {this.state.selfProfile ? (
+                    <Card.Title>
+                      {" "}
+                      {this.props.userInfo.userProfile.username}{" "}
+                    </Card.Title>
+                  ) : (
+                    <Card.Title>
+                      {" "}
+                      {this.props.userInfo.otherUserProfile.username}{" "}
+                    </Card.Title>
+                  )}
                   <Card.Text>
-                    <Typography variant="h4" gutterBottom>
-                      {this.props.userInfo.userProfile.first_name}{" "}
-                      {this.props.userInfo.userProfile.last_name}
-                    </Typography>{" "}
+                    {this.state.selfProfile ? (
+                      <Typography variant="h4" gutterBottom>
+                        {this.props.userInfo.userProfile.first_name}{" "}
+                        {this.props.userInfo.userProfile.last_name}
+                      </Typography>
+                    ) : (
+                      <Typography variant="h4" gutterBottom>
+                        {this.props.userInfo.otherUserProfile.first_name}{" "}
+                        {this.props.userInfo.otherUserProfile.last_name}
+                      </Typography>
+                    )}
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -60,12 +96,33 @@ class Profile extends Component {
           </Grid>
           <Grid item component={Paper}>
             <div className={classes.paper}>
-              <LangTab
-                attendedLang={
-                  this.props.userInfo.userProfile.attended_languages
-                }
-              />
+              {this.state.selfProfile ? (
+                <LangTab
+                  selfProfile={this.state.selfProfile}
+                  attendedLang={
+                    this.props.userInfo.userProfile.attended_languages
+                  }
+                  userProfile={this.props.userInfo.userProfile}
+                />
+              ) : (
+                <LangTab
+                  selfProfile={this.state.selfProfile}
+                  attendedLang={
+                    this.props.userInfo.otherUserProfile.attended_languages
+                  }
+                  userProfile={this.props.userInfo.otherUserProfile}
+                />
+              )}
             </div>
+            <Grid>
+            <div className={classes.paper}>
+              {this.state.selfProfile ? (
+                <Ratings userProfile={this.props.userInfo.userProfile} />
+              ) : (
+                <Ratings userProfile={this.props.userInfo.otherUserProfile} />
+              )}
+              </div>
+          </Grid>
           </Grid>
         </Grid>
       );
@@ -79,14 +136,12 @@ const mapStateToProps = ({ userInfo }) => ({
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      set_user_profile
+      set_user_profile,
+      set_other_user_profile
     },
     dispatch
   );
 
 export default withStyles(styles)(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Profile)
+  connect(mapStateToProps, mapDispatchToProps)(Profile)
 );
--- frontend/src/containers/profile/langTab.js
+++ frontend/src/containers/profile/langTab.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import Statistics from "./statistics";
+import Typography from '@material-ui/core/Typography';
 
-export default function LangTab({attendedLang}) {
-    const [key, setKey] = React.useState('ENGLISH');
-  
+export default function LangTab({selfProfile, attendedLang, userProfile}) {
+    const [key, setKey] = React.useState('');
+
     return (
       <Tabs bg="secondary" text="white"  id="controlled-tab-example" activeKey={key} onSelect={k => setKey(k)}>
-        <Tab eventKey="ENGLISH" title="ENGLISH" disabled={!attendedLang.includes('english')} >
-          <Statistics />
+        <Tab eventKey="ENGLISH" title="ENGLISH" //disabled={!attendedLang.includes('english') }
+         >
+           {selfProfile && <Statistics /> }
         </Tab>
-        <Tab eventKey="TURKISH" title="TURKISH" disabled={!attendedLang.includes('turkish')} >
-        <Statistics />
+        <Tab eventKey="TURKISH" title="TURKISH" //disabled={!attendedLang.includes('turkish') } 
+        >
+           {selfProfile && <Statistics /> }
         </Tab>
-        <Tab eventKey="GERMAN" title="GERMAN" disabled={!attendedLang.includes('german')} >
-        <Statistics />
+        <Tab eventKey="GERMAN" title="GERMAN" //disabled={!attendedLang.includes('german')} 
+        >
+           {selfProfile && <Statistics /> }
         </Tab>
       </Tabs>
     );
