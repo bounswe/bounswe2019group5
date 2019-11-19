8abaa7eb80092e341256c808ea5f5be3d756cfa5
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-11-19 21:03:17 +0300

    Fixed essay upload. The problem was that the code used InputStreamBody's, which caused the Apache http client to use chunked encoding (since it did not know the size of the file beforehand). We can use ByteArrayBody for text essays written within the app. For file uploads, we read the entire file into the memory and again use ByteArrayBody. The backend probably has issues handling file uploads with chunked encoding.

--- languageLearning/app/src/main/java/com/example/languageLearning/UploadEssayActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/UploadEssayActivity.java
@@ -19,15 +19,18 @@ import com.android.volley.Response;
 
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.apache.http.entity.mime.content.InputStreamBody;
 import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.Serializable;
@@ -48,6 +51,20 @@ public class UploadEssayActivity extends AppCompatActivity {
     ImageView checkCircleView;
     Button finishButton;
     TextView textView;
+
+
+
+    byte[] isToByteArray(InputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        while (true) {
+            int r = in.read(buffer);
+            if (r == -1) break;
+            out.write(buffer, 0, r);
+        }
+
+        return out.toByteArray();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +91,7 @@ public class UploadEssayActivity extends AppCompatActivity {
         textParams.put("reviewer", reviewerProfile.username);
         FormBodyPart[] filePart = new FormBodyPart[1];
         if (essayText != null) {
-            filePart[0] = new FormBodyPart("writing", new InputStreamBody(new ByteArrayInputStream(essayText.getBytes(StandardCharsets.UTF_8)), ContentType.TEXT_PLAIN, "essay.txt"));
+            filePart[0] = new FormBodyPart("writing", new ByteArrayBody(essayText.getBytes(StandardCharsets.UTF_8), ContentType.TEXT_PLAIN, "essay.txt"));
         }
         else {
             try {
@@ -82,11 +99,18 @@ public class UploadEssayActivity extends AppCompatActivity {
                 result.moveToFirst();
                 String fileName = result.getString(result.getColumnIndexOrThrow("_display_name"));
                 String mimeType = result.getString(result.getColumnIndexOrThrow("mime_type"));
-                filePart[0] = new FormBodyPart("writing", new InputStreamBody(getContentResolver().openInputStream(Uri.parse(essayPath)), ContentType.create(mimeType), fileName));
+
+                filePart[0] = new FormBodyPart("writing", new ByteArrayBody(isToByteArray(getContentResolver().openInputStream(Uri.parse(essayPath))), ContentType.create(mimeType), fileName));
             }
             catch (FileNotFoundException e) {
                 e.printStackTrace();
                 Toast.makeText(this, "File not found", Toast.LENGTH_SHORT).show();
+                finish();
+                return;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error reading the file", Toast.LENGTH_SHORT).show();
                 finish();
                 return;
             }
