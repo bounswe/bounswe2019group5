a330091fffafed5f7579d9f51cf9a10df201db6b
Selamettin Dirik <selamettinn.95@gmail.com>
2019-11-20 22:56:46 +0300

    listening-test action-creator is created

--- frontend/src/redux/action-creators/exercises.js
+++ frontend/src/redux/action-creators/exercises.js
@@ -2,11 +2,14 @@ import {
   VOCABULARY_TEST_REQUESTED,
   VOCABULARY_TEST,
   GRAMMAR_TEST_REQUESTED,
-  GRAMMAR_TEST
+  GRAMMAR_TEST,
+  LISTENING_TEST_REQUESTED,
+  LISTENING_TEST
 } from "../actions";
 
 import { get_vocabulary_test as get_vocabulary_test_api } from "../../api/exercises";
 import { get_grammar_test as get_grammar_test_api } from "../../api/exercises";
+import { get_listening_test as get_listening_test_api } from "../../api/exercises";
 
 export const get_vocabulary_test = (token, selectedLanguage) => {
   return dispatch => {
@@ -31,6 +34,20 @@ export const get_grammar_test = (token, selectedLanguage) => {
       dispatch({
         type: GRAMMAR_TEST,
         grammarTest: response
+      });
+    });
+  };
+};
+
+export const get_listening_test = (token, selectedLanguage) => {
+  return dispatch => {
+    dispatch({
+      type: LISTENING_TEST_REQUESTED
+    });
+    get_listening_test_api(token, selectedLanguage).then(response => {
+      dispatch({
+        type: LISTENING_TEST,
+        listeningTest: response
       });
     });
   };
