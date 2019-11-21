87ffbe477caff9577694f51ad8ef77ea4e6e77f3
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2019-11-21 21:20:12 +0300

    The bridge activity now actually changes the language. It also redirects the user to take a proficiency exam if he has not solved any in the new language yet.

--- languageLearning/app/src/main/java/com/example/languageLearning/MainMenuActivity.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/MainMenuActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -88,8 +89,12 @@ public class MainMenuActivity extends AppCompatActivity {
         });
 
         welcomeMessage.setText("Hello " + app.getUsername() + "!");
-        currentLanguageView.setText(app.getLanguage().toUpperCase());
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currentLanguageView.setText(app.getLanguage().toUpperCase());
     }
 
     public void showExerciseSelectPopup(){
