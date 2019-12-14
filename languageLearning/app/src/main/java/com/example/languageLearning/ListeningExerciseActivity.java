d0c02067303803b8763f15a4da6e898bcd57a89c
tony <hasan.ozturk@boun.edu.tr>
2019-12-14 11:49:56 +0300

    Create a standalone activity for listening exercise and design its UI

---
+++ languageLearning/app/src/main/java/com/example/languageLearning/ListeningExerciseActivity.java
@@ -0,0 +1,14 @@
+package com.example.languageLearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeningExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listening_exercise);
+    }
+}
