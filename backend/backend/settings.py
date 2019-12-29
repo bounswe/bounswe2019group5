a54407cb4e3b8deb7a06eaa4c64079ef75ccab50
furkan-kadioglu <furkan.kadioglu@boun.edu.tr>
2019-12-29 12:56:26 +0300

    db_dumb added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
