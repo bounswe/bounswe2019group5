ea640cd6c90f2a51b49d964bece9c97bf755fd3f
ali-deser-2016400261 <meric.deser@boun.edu.tr>
2020-01-01 22:09:44 +0300

    Progress result page variable holding

--- languageLearning/app/src/main/java/com/example/languageLearning/AppProgressResult.java
+++ languageLearning/app/src/main/java/com/example/languageLearning/AppProgressResult.java
@@ -6,7 +6,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.os.Bundle;
 public class AppProgressResult extends AppCompatActivity {
-    TextView progressView, gradeView;
+    TextView progressView;
     TextView progressLabel2;
     ProgressBar progressBar;
 
@@ -15,14 +15,12 @@ public class AppProgressResult extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_progress_resullt);
         progressView = findViewById(R.id.progressPercentage);
-        gradeView = findViewById(R.id.totalGrade);
         progressLabel2 = findViewById(R.id.progressLabel2);
         String progress = getIntent().getStringExtra("progress");
         String language = getIntent().getStringExtra("language");
         Double progressDouble = getIntent().getDoubleExtra("progressDouble",0.0);
-        String totalGrade = getIntent().getStringExtra("total_grade");
+        Double totalGrade = getIntent().getDoubleExtra("total_grade",0.0);
         progressView.setText(progress);
-        gradeView.setText(totalGrade);
         progressLabel2.setText("of the "+language.substring(0,1).toUpperCase()+language.substring(1)+" exercises");
         progressBar = findViewById(R.id.appProgressBar);
 
